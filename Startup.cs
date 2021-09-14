8a632ffa9de38af83c49e8821ef584473aa920c3
Andy <andygrecu2000@gmail.com>
2021-09-14 11:01:26 +0300

    Duie Mocker

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace Catalog
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
 
-            services.AddHealthChecks().AddMongoDb(mongoDbSettings.ConnectionString, name: "mongodb", timeout: TimeSpan.FromSeconds(3),tags:new[]{"ready"});
+            services.AddHealthChecks().AddMongoDb(mongoDbSettings.ConnectionString, name: "mongodb", timeout: TimeSpan.FromSeconds(3), tags: new[] { "ready" });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -68,7 +68,11 @@ namespace Catalog
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
             }
 
-            app.UseHttpsRedirection();
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+
 
             app.UseRouting();
 
@@ -78,29 +82,33 @@ namespace Catalog
             {
                 endpoints.MapControllers();
 
-                endpoints.MapHealthChecks("/health/ready",new HealthCheckOptions{
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
                     Predicate = (check) => check.Tags.Contains("ready"),
-                    ResponseWriter=async(context,report)=>
-                    {
-                        var result = JsonSerializer.Serialize(
-                            new{
-                                status = report.Status.ToString(),
-                                checks = report.Entries.Select(entry=> new {
-                                    name =entry.Key,
-                                    status = entry.Value.ToString(),
-                                    exception = entry.Value.Exception != null?entry.Value.Exception.Message:"none",
-                                    duration = entry.Value.Duration.ToString()
-                                })
-                            }
-                        );
-                        context.Response.ContentType=MediaTypeNames.Application.Json;
-                        await context.Response.WriteAsync(result);
-                    }
+                    ResponseWriter = async (context, report) =>
+                      {
+                          var result = JsonSerializer.Serialize(
+                              new
+                              {
+                                  status = report.Status.ToString(),
+                                  checks = report.Entries.Select(entry => new
+                                  {
+                                      name = entry.Key,
+                                      status = entry.Value.ToString(),
+                                      exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                      duration = entry.Value.Duration.ToString()
+                                  })
+                              }
+                          );
+                          context.Response.ContentType = MediaTypeNames.Application.Json;
+                          await context.Response.WriteAsync(result);
+                      }
                 });
-                endpoints.MapHealthChecks("/health/live",new HealthCheckOptions{
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+                {
                     Predicate = (_) => false
                 });
-                
+
             });
         }
     }
